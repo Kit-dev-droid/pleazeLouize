9e6f672b4c885c9a989062a793bb1241f4c4f163
kyle15451 <kyle.kitchener@outlook.com>
2025-10-06 06:07:54 +0200

    added default home component routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home-component/home-component';
 export const routes: Routes = [
+     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent}
 ];
