004a4b5c33c5171cae08f1c08539a8d7b4e6ddfd
kyle15451 <kyle.kitchener@outlook.com>
2025-10-08 06:26:58 +0200

    upldated routing to include the footer in the layout component

--- src/app/layout-component/layout-component.ts
+++ src/app/layout-component/layout-component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBar } from '../shared/navigation-bar/navigation-bar';
+import { FooterComponent } from '../shared/footer-component/footer-component';
 
 @Component({
   selector: 'app-layout-component',
-  imports: [RouterOutlet,NavigationBar],
+  standalone: true,
+  imports: [RouterOutlet,NavigationBar,FooterComponent],
   templateUrl: './layout-component.html',
   styleUrl: './layout-component.scss'
 })
