b6db11a6c8d16fad0fabd4292ee3516559a0a386
kyle15451 <kyle.kitchener@outlook.com>
2025-10-09 06:42:31 +0200

    basic lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home-component/home-component';
 import { ContactComponent } from './features/contact-component/contact-component';
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    {path: 'home', component: HomeComponent},
-    {path: 'contact', component: ContactComponent}
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home-component/home-component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./features/contact-component/contact-component').then(m => m.ContactComponent)
+    },
+    {
+    path: '',
+    component: HomeComponent
+  }
 ];
