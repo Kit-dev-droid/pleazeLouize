ef7ac2da3a23e85a02e03196ab17d9bc854cca73
kyle15451 <kyle.kitchener@outlook.com>
2025-10-16 06:30:43 +0200

    basic contact form with no functionality

--- src/app/features/contact/contact-component/components/contact-component.ts
+++ src/app/features/contact/contact-component/components/contact-component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject  } from '@angular/core';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageModule, Message } from 'primeng/message';
+import { MessageService } from 'primeng/api';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-component',
-  imports: [],
+  imports: [FloatLabelModule, InputTextModule, ReactiveFormsModule, ButtonModule, ToastModule, Message],
   templateUrl: './contact-component.html',
+  providers: [MessageService],
   styleUrl: './contact-component.scss'
 })
 export class ContactComponent {
+
+    messageService = inject(MessageService);
+
+    contactForm: FormGroup;
+
+    formSubmitted = false;
+
+     loading: boolean = false;
+
+    load() {
+        this.loading = true;
+
+        setTimeout(() => {
+            this.loading = false
+        }, 2000);
+    }
+    
+    constructor(private fb: FormBuilder) {
+        this.contactForm = this.fb.group({
+            name: ['', Validators.required],
+            surname: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            phoneNumber: ['', [Validators.required]],
+            message: ['', [Validators.required]]
+        });
+    }
+
+    onSubmit() {
+        this.formSubmitted = true;
+        if (this.contactForm.valid) {
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form Submitted', life: 3000 });
+            this.contactForm.reset();
+            this.formSubmitted = false;
+        }
+    }
+
+    isInvalid(controlName: string) {
+        const control = this.contactForm.get(controlName);
+        return control?.invalid && (control.touched || this.formSubmitted);
+    }
+
+
 
 }
